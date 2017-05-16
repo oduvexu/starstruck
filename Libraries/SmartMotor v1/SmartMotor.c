ba70062a888d8d5a5601b6135f687e2c8f74af59
Satoius58 <satoius58@yahoo.com>
2017-05-15 22:55:16 -0400

    Update SmartMotor.c

--- Libraries/SmartMotor v1/SmartMotor.c
+++ Libraries/SmartMotor v1/SmartMotor.c
@@ -1,71 +1,48 @@
 /*
-
 Updated: 4/30/2017
-
 Basic Instructions:
       Thank you for using the smart motor library made at ODU as an attempt to reduce the over heating of motors. The method used is to predict the amps and limit the effort in respect to amps.
       Anyone of the settings in the init_SmartMotor initializer can be set to how you want such as amp limit, rate of decrease in effort, rate of increase in effort etc...
-
       ---------Step 1: Initialize Smart Motors------------------
       In the main function, initialize all the motors you want to be limited by calling the init_SmartMotor function so...
-
       init_SmartMotor(smart_motor[Wheel1], Wheel1);
       init_SmartMotor(smart_motor[Wheel2], Wheel2);
-
       where "Wheel" is what you called the motor when you created it. It uses the same value that you created for the motor to convert it into a Smart Motor.
-
       ---------Step2: Begin the Smart Motor Task-----------------------------------
       Once you've initialized the Smotor, you will create a task for every motor. so...
-
       //declare the tasks
       task smotor1;
       task smotor2;
-
       //define the tasks
       task smotor1{
       start_SmartMotor(smart_motor[Wheel1]);
       }
-
       task smotor2{
       start_SmartMotor(smart_motor[Wheel2]);
       }
-
       //In your main function, start the tasks
       startTask(smotor1);
       startTask(smotor2);
-
       ---------Step3: Change any settings if default isn't desired-----------------
       You can change various settings if you'd like
-
       //Change amp limit, 2 amps is default
       smart_motor[Wheel1].targetAmp = 2;
-
       //Change amp increase when under amp limit,3 is default
       smart_motor[Wheel1].increaseAmpRate = 3;
-
       //Change amp decrease when over amp limit, 8 is default
       smart_motor[Wheel1].reduceAmpRate = 8;
-
       ---------Step4: Multiply by the multiplier-----------------------------------
       When you us the Smart Motor Library you no long equate the motor[] with the input. All you have to do is add an "S" at the end and put a .input so smotor[].input . This will activate all the calculations needed to limit the motors.
       If you want to limit the motors however you need to multiply by the multiplier. so...
-
       motor[TEST] = vexRT[CH1];
-
       becomes
-
       smart_motor[TEST].input = vexRT[CH1] * smart_motor[TEST].multiplier;
-
-
-
 Grouping Motors:
          We can also set the limit of groups of motors. This is to protect the cortex itself from over heating. Ports 1-5, 6-10 and the expander all share it's own
          respected PTC (Positive Temperature Coefficient) fuse. It's a good idea to limit the entire group of motors on each set of ports to some amps as a whole.
          Grouping motors is extremely easy. First you need to set all the motors to Smart Motors as stated above, then add them to groups. Once they're in groups you can multiply by
          the group multiplier and the set will be changed equally. This is optimal for drive systems where you don't want to reduce the effort unevenly.
-
        ---------Step1: Add motor to group------------------------------------------
-
        //Adds 3 motors to group 0
        group_SmartMotor(0,smart_motor[Wheel1]);
        group_SmartMotor(0,smart_motor[Wheel2]);
@@ -73,14 +50,10 @@ Grouping Motors:
        
        //Monitor group 0
        monitorGroup[0] = true;
-
        //Set multiplier
-
       smart_motor[Wheel1].input = vexRT[CH1] * groupMultiplier[0];
       smart_motor[Wheel2].input = vexRT[CH1] * groupMultiplier[0];
       smart_motor[Wheel3].input = vexRT[CH1] * groupMultiplier[0];
-
-
 When motor is moving same direction as effort than limit normally
 When motor is moving opposite direction as effort, don't allow negative effort to allow more than negative of amp limit
 */
@@ -149,13 +122,13 @@ void init_SmartMotor(SmartMotor &sm, int smotor)
 
 SmartMotor smart_motor[12];
 int groups[12][12] = {{0}};
-int groupAmount[12] = {0};
-int groupGhostAmount[12] = {0};
-float groupAmps[12] = {0};
-float groupDelta[12] = {0};
-float groupMultiplier[12] = {1.0};
-float groupAmpsLimit[12] = {4.0};
-bool monitorGroup[12] = {false};
+int groupAmount[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
+int groupGhostAmount[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
+float groupAmps[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
+float groupDelta[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
+float groupMultiplier[12] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+float groupAmpsLimit[12] = {4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0};
+bool monitorGroup[12] = {false, false, false, false, false, false, false, false, false, false, false, false};
 int check = 0;
 
 void group_SmartMotor(int group, SmartMotor &sm){
@@ -203,9 +176,9 @@ void start_SmartMotor(SmartMotor &sm){
 				for(int i = 0; i < groupAmount[j] + 1; i++){
 					calcAmp = calcAmp + smart_motor[groups[j][i]].amps;
 					if((abs(groupAmps[j]) > groupAmpsLimit[j]) && groupDelta[j] < 127)
-						groupDelta[j] = groupDelta[j] + 5;
+						groupDelta[j] = groupDelta[j] + 0.1;
 					else if((abs(groupAmps[j]) < groupAmpsLimit[j]) && groupDelta[j] > 0)
-						groupDelta[j] = groupDelta[j] - 1;
+						groupDelta[j] = groupDelta[j] - 0.05;
 					else if(motor[sm.smotor] == 0)
 						groupDelta[j] = 0;
 				}
@@ -311,7 +284,7 @@ void start_SmartMotor(SmartMotor &sm){
 
 			//If Amps go above target and the limiter is within bounds, deduct effort by 5 every 100msec
 			if((abs(sm.targetAmp) < abs(sm.amps)) && sm.deltaEffort < 127){
-					sm.deltaEffort = sm.deltaEffort + 5;
+					sm.deltaEffort = sm.deltaEffort + 5.0;
 			}
 
 			//If Amps are below target and the limiter is within bounds, slowly allow more effort by 1 every 100msec
@@ -328,7 +301,7 @@ void start_SmartMotor(SmartMotor &sm){
 				motor[sm.smotor] = sm.input;
 
 			else if((sm.direction * sm.input) <= -1 && getMotorVelocity(sm.smotor) > 0){
-				motor[sm.smotor] = sm.input*(1/(getMotorVelocity(sm.smotor)*sm.Ke*2));
+				motor[sm.smotor] = sm.input*(1/(getMotorVelocity(sm.smotor)*sm.Ke));
 
 			}
 
